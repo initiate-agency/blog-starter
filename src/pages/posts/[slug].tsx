58cef355dc45fe6f081265b9b578467e37b27260
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-03-29 05:51:46 -0600

    add cool parallax effect

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -11,13 +11,14 @@ type PostPageProps = {
 export default function PostPage({ post }: PostPageProps) {
   return (
     <article>
-      <Container className="prose my-12 lg:prose-xl sm:my-16">
+      <Container className="prose my-8 lg:prose-xl sm:my-12">
         <h1>{post.title}</h1>
       </Container>
-      <div className="h-[250px] w-full shadow-xl ring-1 ring-neutral-900/10 lg:h-[500px]">
-        <img src={post.feature_image || ''} className="h-full w-full object-cover" alt="" />
-      </div>
-      <Container className="prose my-12 lg:prose-xl sm:my-16">
+      <div
+        className="h-[250px] w-full bg-cover bg-fixed bg-center bg-no-repeat shadow-lg ring-1 ring-neutral-900/10 md:h-[400px] lg:h-[500px]"
+        style={{ backgroundImage: `url(${post.feature_image || ''})` }}
+      ></div>
+      <Container className="prose my-8 lg:prose-xl sm:my-12">
         <time dateTime={post.published_at || ''} className="italic text-neutral-500">
           Published on{' '}
           {post.published_at
